cdb1304de41f23ffd49b3f261f1bcd9a8749253f
phantruong <phanvantruong10042001@gmail.com>
2023-03-16 13:49:01 +0700

    first commit

---
+++ src/main/java/fpt/edu/vn/api/NewAPI.java
@@ -0,0 +1,15 @@
+package fpt.edu.vn.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class NewAPI {
+    @GetMapping("/test")
+
+    public String testAPI() {
+
+        return "success";
+
+    }
+}
